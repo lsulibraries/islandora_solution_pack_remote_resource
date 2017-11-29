a532c819d516a09dcbf18bb0417556fc51023186
Jason Peak <jpeak5@lsu.edu>
2017-11-28 20:40:49 -0600

    adds capability for resumptionToken...

--- modules/islandora_remote_resource_batch/includes/IslandoraRemoteResourceOaiDoraBatchPreprocessor.php
+++ modules/islandora_remote_resource_batch/includes/IslandoraRemoteResourceOaiDoraBatchPreprocessor.php
@@ -3,7 +3,7 @@
 class IslandoraRemoteResourceOaiDoraBatchPreprocessor extends IslandoraRemoteResourceListBatchPreprocessor {
 
   protected function getUrlList() {
-    $oaiListUrl = sprintf('%s?verb=ListRecords&metadataPrefix=mods&set=%s', $this->parameters['oai_endpoint'], $this->parameters['oai_set']);
+    $oaiListUrl = sprintf('%s?verb=ListIdentifiers&metadataPrefix=mods&set=%s', $this->parameters['oai_endpoint'], $this->parameters['oai_set']);
     $recordsList = file_get_contents($oaiListUrl);
     $xml = simplexml_load_string($recordsList);
     $xml->registerXPathNamespace('o', "http://www.openarchives.org/OAI/2.0/");
@@ -12,11 +12,22 @@ class IslandoraRemoteResourceOaiDoraBatchPreprocessor extends IslandoraRemoteRes
     $urls = array();
     $remote_host = $this->parameters['oai_remote_host'];
 
-    foreach ($identifiers as $id) {
-      $i = $this->parameters['oai_pid_index'];
-      $pid = explode(':', $id)[$i];
-      $urls[] = sprintf("%s/islandora/object/%s", $remote_host, preg_replace('/_/', ':', $pid));
-    }
+    do {
+      foreach ($identifiers as $id) {
+        $i = $this->parameters['oai_pid_index'];
+        $pid = explode(':', $id)[$i];
+        $urls[] = sprintf("%s/islandora/object/%s", $remote_host, preg_replace('/_/', ':', $pid));
+      }
+
+      $resumptionToken = $xml->xpath('//o:resumptionToken');
+      if($resumptionToken) {
+        $resumeUrl = sprintf('%s?verb=ListIdentifiers&resumptionToken=%s', $this->parameters['oai_endpoint'], $resumptionToken);
+        $recordsList = file_get_contents($resumeUrl);
+        $xml = simplexml_load_string($recordsList);
+        $xml->registerXPathNamespace('o', "http://www.openarchives.org/OAI/2.0/");
+        $identifiers = $xml->xpath('//o:identifier');
+      }
+    } while ($resumptionToken);
     return $urls;
   }
 }
